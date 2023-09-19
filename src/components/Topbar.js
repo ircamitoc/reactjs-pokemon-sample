c01d9221b5ae58e882c4ff26c20c73da770a9187
ircamitoc <iraincamitoc@gmail.com>
2023-09-20 01:09:12 +0800

    Updated files

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,7 +1,34 @@
-import React from "react";
+import "./Topbar.css";
+import pokeball from "../images/pokeball.png";
+import pokeballColor from "../images/pokeballColor.png";
 
 function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className="title">
+      <div className="title__left">
+        <p>Pokedex</p>
+        <div className="caught-seen">
+          <div className="caught">
+            <img
+              src={pokeballColor}
+              alt="pokeball"
+              style={{ width: "30px", marginRight: "10px" }}
+            />
+            <p>438</p>
+          </div>
+          <div className="seen">
+            <img
+              src={pokeball}
+              alt="pokeball"
+              style={{ width: "30px", marginRight: "10px" }}
+            ></img>
+            <p>649</p>
+          </div>
+        </div>
+      </div>
+      <p style={{ color: "white" }}>A -&gt; Z</p>
+    </div>
+  );
 }
 
 export default Topbar;
